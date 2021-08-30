c0a85c7fd7c6a12a1d6675d53b309d58470a83f9
Jonathan Cano <jcano528@gmail.com>
2021-08-30 18:50:49 -0500

    tested the questions and app works so far

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is your projects name?',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('please enter a value for your project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'purpose',
+            message: 'What is the purpose of your project?',
+            default: 'A webite'
+        },
+        {
+            type: 'checkbox',
+            name: 'languagesBuiltWith',
+            message: 'What languages were used to build this website?',
+            choices: ['javascript', 'html', 'css', 'web api', 'node.js']
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +39,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+promptUser();
